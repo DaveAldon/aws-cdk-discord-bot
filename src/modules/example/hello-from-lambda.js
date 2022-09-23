1e0433cfabf477924eb67ea688e89138f33389f1
DaveAldon <crawford@mail.gvsu.edu>
2022-09-23 11:52:39 -0400

    Updated to fix deploy issues

--- src/modules/example/hello-from-lambda.js
+++ src/modules/example/hello-from-lambda.js
@@ -2,23 +2,23 @@
  * A Lambda function that replies to interaction with static string
  */
 
-const { globalHandler } = require('../handler.js')
+const { globalHandler } = require('../handler.js');
 
 exports.data = {
   name: 'hello',
   type: 1,
-  description: 'replies with hello world.'
-}
+  description: 'replies with hello world.',
+};
 
-const action = async (body) => {
+const action = async body => {
   // May do something here with body
   // Body contains Discord command details
   let response = {
-    "content": "Hello from Lambda!"
-  }
-  return response
-}
+    content: 'Hello from Bravo Lambda!',
+  };
+  return response;
+};
 
-exports.handler = (event) => {
-  globalHandler(event, action)
-}
+exports.handler = event => {
+  globalHandler(event, action);
+};
